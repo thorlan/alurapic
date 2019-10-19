86c295c23349224cecceae730f26c38d15177aa0
thiago orlandini <thiagorlandini@hotmail.com>
2019-10-19 15:54:36 -0300

    blalba

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,12 +5,19 @@ import { CommonModule } from '@angular/common';
 import { PhotoComponent } from "./photo/photo.component";
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { LoadComponent } from './photo-list/load/load.component';
 
+// component e pipe precisam ser delcarados
 @NgModule({
     declarations: [ 
         PhotoComponent, 
         PhotoListComponent, 
-        PhotoFormComponent 
+        PhotoFormComponent, 
+        PhotosComponent,
+        FilterByDescription,
+        LoadComponent
     ], // tipo private
     imports: [ 
         HttpClientModule,
